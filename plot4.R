2a5ae4ac33633df1b49036a91f7ddc6024658cea
anngan <anna.gandziarska@gmail.com>
2017-06-11 11:32:33 +0100

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,22 @@
+#Setting directory to the one in which the downloaded file for the project can be found. Loading the required package.
+setwd("C:/Users/Name/Desktop/datasciencecoursera")
+library("data.table")
+
+#Reading and organizing data: changing the date type, using POSIXct for date related functions, choosing the dates.
+power1 <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+#Creating the file consisting of 4 graphs and saving it into a png file.
+png("plot4.png", width=480, height=480)
+par(mfrow=c(2,2))
+plot(powerDT[, dateTime], powerDT[, Global_active_power], type="l", xlab="", ylab="Global Active Power")
+plot(powerDT[, dateTime],powerDT[, Voltage], type="l", xlab="datetime", ylab="Voltage")
+plot(powerDT[, dateTime], powerDT[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
+lines(powerDT[, dateTime], powerDT[, Sub_metering_2], col="red")
+lines(powerDT[, dateTime], powerDT[, Sub_metering_3],col="blue")
+legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "), lty=c(1,1), bty="n", cex=.5) 
+plot(powerDT[, dateTime], powerDT[,Global_reactive_power], type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
