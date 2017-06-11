257e2609a0ca0880dace39cadcc4c9ee64a33116
anngan <anna.gandziarska@gmail.com>
2017-06-11 11:26:04 +0100

    Create plot1.R

--- plot1.R
+++ plot1.R
@@ -8,7 +8,7 @@ power1[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_act
 power1[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
 power1 <- power1[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
 
-#Creating the graph.
+#Creating the graph and saving it into a png file.
 png("plot1.png", width=480, height=480)
 hist(powerDT[, Global_active_power], main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 dev.off()
